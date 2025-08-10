e1e7eb73e1edf1902a6818102c99cd10f107f1d9
Au <au.perspectives@gmail.com>
2025-08-11 01:54:53 +0700

    Initialize first 3 challenges.

---
+++ Exam/Exam01/INumberGenerator.cs
@@ -0,0 +1,33 @@
+﻿namespace Exam01;
+
+internal interface INumberGenerator
+{
+    /// <summary>
+    /// Generates a sequence of random integers.
+    /// </summary>
+    /// <returns>Sequence of random integers.</returns>
+    IEnumerable<int> Random();
+}
+
+internal class NumberGenerator : INumberGenerator
+{
+    private readonly Random _random;
+    public NumberGenerator()
+        => _random = new Random();
+
+    public static INumberGenerator Create()
+        => new NumberGenerator();
+
+    /// <summary>
+    /// Generates a sequence of random integers.
+    /// </summary>
+    /// <returns>Sequence of random integers.</returns>
+    public IEnumerable<int> Random()
+    {
+        var range = _random.Next(1, 7);
+        while (range-- > 0)
+        {
+            yield return _random.Next(1, 100);
+        }
+    }
+}
---
+++ Exam/Exam01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam01;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        IEnumerable<int> numbers = NumberGenerator.Create().Random();
+        // TODO: Your solution goes here
+    }
+}
